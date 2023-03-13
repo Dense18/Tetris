074a0a2809250833d9991cdea8842096c738d78a
George <george-andreas@hotmail.com>
2023-03-13 11:03:43 -0700

    added vectors for positions

--- model/Block.py
+++ model/Block.py
@@ -1,9 +1,17 @@
+import pygame
+from settings import *
+
 class Block:
     """
         Stores information of each block in the tetromino
     """
-    def __init__(self):
-        pass
+    def __init__(self, tetromino, pos):
+        self.tetromino = tetromino
+        self.pos = vec(pos) + INITIAL_TETROMINO_OFFSET
+        self.rect = pygame.Rect((self.pos * BLOCK_SIZE),(BLOCK_SIZE, BLOCK_SIZE))
+        
 
-    def draw(self):
+    def draw(self, screen):
+        # pygame.draw.rect(screen, "orange", self.rect, border_radius=8)
+        pygame.draw.rect(screen, "orange", ((self.pos * BLOCK_SIZE),(BLOCK_SIZE, BLOCK_SIZE)), border_radius=8)
         pass
