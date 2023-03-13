7ab2af286bbfb9af0894105ea2f933ffa6053c60
George <george-andreas@hotmail.com>
2023-03-13 13:17:05 -0700

    added custom event for animation speed

--- App.py
+++ App.py
@@ -11,7 +11,19 @@ class App:
         self.isRunning = True
         self.tetris = Tetris(self)
         self.clock = pygame.time.Clock()
+
+        self.set_animation_event()
         pass
+
+    def set_animation_event(self):
+        self.animation_event = pygame.USEREVENT + 1
+        self.speed_event = pygame.USEREVENT + 2
+
+        self.animation_flag = False
+        self.accelerate_event = True
+
+        pygame.time.set_timer(self.animation_event, ANIMATION_INTERVAL)
+        pygame.time.set_timer(self.accelerate_event, ACCELERATE_INTERVAL)
     
     def loop(self):
         self.clock.tick(FPS)
@@ -23,9 +35,14 @@ class App:
         self.events = pygame.event.get()
 
     def update(self):
+        self.animation_flag = False
         for event in self.events:
             if event.type == pygame.QUIT:
                 self.isRunning = False
+            if event.type == self.animation_event:
+                self.animation_flag = True
+            if event.type == self.accelerate_event:
+                self.accelerate_event = True
         self.tetris.update(self.events)
         pass
 
