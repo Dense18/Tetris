b606eec4cab3c5349d59fa0f086469b80e9c3f5b
George <george-andreas@hotmail.com>
2023-03-13 14:39:20 -0700

    added init to pygame

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from App import App
 def main():
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Tetris")
+    pygame.init()
 
     app = App(screen)
     app.run()
