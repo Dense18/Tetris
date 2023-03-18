1474181ce57c29d925e33dbc036fa3ffabcc5562
George <george-andreas@hotmail.com>
2023-03-17 18:01:56 -0700

    add file extension for saving system

--- SaveLoadSystem.py
+++ SaveLoadSystem.py
@@ -3,11 +3,12 @@ import os
 
 
 class SaveLoadSystem():
-    def __init__(self, file_path):
+    def __init__(self, file_path, file_extension = "json"):
         self.file_path = file_path
+        self.file_extension = file_extension
     
     def save(self, data, file_name):
-        with open(os.path.join(self.file_path, file_name), "w") as file:
+        with open(self.get_full_path(file_name), "w") as file:
             json.dump(data, file)
     
     def load(self, file_name):
@@ -18,7 +19,7 @@ class SaveLoadSystem():
             return None
     
     def get_full_path(self, file_name):
-        return os.path.join(self.file_path, file_name)
+        return os.path.join(self.file_path, file_name + "." + self.file_extension)
     
     def is_file_exist(self, file_name):
         return os.path.exists(self.get_full_path(file_name)) \
