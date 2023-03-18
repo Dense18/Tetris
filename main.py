7ea023ebe75eed4caa35e34b45292dbdedd30494
George <george-andreas@hotmail.com>
2023-03-18 09:02:00 -0700

    intitialize mixer on main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from settings import *
 def main():
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Tetris")
+    pygame.mixer.pre_init(44100, -16, 2, 2048)
+    pygame.mixer.init()
     pygame.init()
 
     app = App(screen)
